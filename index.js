067767941b9a03848b311834a761bca21ad3fe35
Yaşar Muslu <yasarmuslu93@outlook.com>
2023-03-22 11:32:48 +0300

    S1D2 Proje

--- index.js
+++ index.js
@@ -1,170 +1,106 @@
-/* Görev 1: Isınalım Biraz :) */
 
-/* TÜM CEVAPLARINI BİR FUNCTION İÇİNDE VERDİĞİNİZ EMİN OL (GÖREV 1D - GÖREV 6). AKSİ TAKTİRDE, OTOMATİK TEST SİSTEMİ ÇALIŞMAYACAKTIR. */
-
-// Görev 1a, 1b ve 1c puanlamaya dahil değildir
-
-/*
-Bu görevleri yaparken, yazdığın kod'un doğru çıktı verdiğini kontrol etmek için console.log'u kullanmanı öneririz.
-Bunun için cevabı direk console'a logla veya çağırdığın fonksiyondan dönen değeri console'a logla.  
-Örneğin:  console.log(ornekFonksiyon(deger1,deger2))
-*/
-
-/*
-Görev 1a - Ehliyet Yaşı (puanlamaya dahil değildir)
-
-Aşağıdakileri yap:   
-   1. surucuYasi isimli bir değişken yaz ve bir sayı ata.
-   2. Eğer yaş 18'den büyük ise Console'da true, değilse false değeri yazdırın.
-
-   İPUCU: fonksiyon oluşturmaya gerek yok
-*/
-
-/*
-Görev 1b - Değerler (puanlamaya dahil değildir)
-
-Aşağıdakileri yap:   
-   1. 2 değişken tanımla ve bunlara değerler ata (isimlendirme olarak birinciDeger ikinciDegeri kullanabilirsin)
-   2. 1. değeri 2. değer ile karşılaştıran bir koşul oluşturun
-   3. 2. adımdaki koşul doğru olduğunda 1. değeri değiştirin
-   4. 1. değeri Console'a yazdırın
-
-   İPUCU: fonksiyon oluşturmaya gerek yok
-*/
-
-/*
-Görev 1c - String bir değeri Number'a dönüştürün (puanlamaya dahil değildir)
-
-Aşağıdakileri yap:   
-   1. String tipinde ve değeri "1999" olan bir değişken oluşturun
-   2. Değeri "1999" olan bu String'i değeri 1999 olan bir integer'a dönüştürün
-   3. Sonucu Console'a yazdırın
-
-   İPUCU: Number metoduna bakabilirsin
-*/
-
-/*
-Görev 1d - Çarpma
- 
-Aşağıdakileri yaparak carpma isimli fonksiyonu tamamla:   
-   1. 2 argüman alsın(a ve b olarak tanımlayabilirsin)
-   2. a ve b'yi çarpıp, sonucu dönsün
-   3. console.log(carpma(7,4)) ile yazdığın fonsiyonu test edin. Console'da sonucu 28 olarak görmelisin.
-*/
-
-function carpma(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+let surucuYasi = 19
+if(surucuYasi>18){
+  console.log(true)
+}else{
+  console.log(false)
 }
 
-/* Görev 2 : Köpeğin Yaşı */
 
-/*
-Aşağıdakileri yap:
- 1. Aşağıdaki kopeginYasi fonsiyonunu bir argüman alacak şekilde tanımla.
- 2. Gelen değeri kullanarak köpeğin yaşını hesapla (insanlarda 1 yıl köpeklerde 7 yıla denk gelir)
- 3. Hesaplanan köpeğin yaşını dönün.
- */
+let birinciDeger = 10
+let ikinciDeger = 20
+if(ikinciDeger > birinciDeger){
+  birinciDeger = 15
+}
+console.log(birinciDeger);
 
-function kopeginYasi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+
+let x = "1999"
+let num = parseInt(x)
+console.log(x);
+
+function carpma(a,b) {
+ return a * b;
 }
 
-/* Görev 3 */
-/*
-Aşağıdakileri oyun isimli fonksiyonu kullanarak yap.
-1. 2 argüman alın: oyuncunun tercihini alan bir string değer: "Taş", "Kağıt" veya "Makas" 
-   ve bilgisayarınkini alan: "Taş", "Kağıt" veya "Makas"
-   Not: string değerlerin yazımına dikkat! İlk harf büyük ve türkçe karakterler kullanarak. Yoksa testi geçemez.
-2. Aşağıdaki oyun kurallarına göre oyuncunun kazanıp kazanamadığını veya berabere kalıp kalmadığını dönün - (uygulamanın oluşturduğu çıktı tam olarak aşağıdakinin aynısı olmalı)
- - kazanma durumunda "Kazandın!" dönmeli
- - kaybetme durumunda "Kaybettin!" dönmeli
- - beraberlikte "Beraberlik" dönmeli
 
-OYUNUN KURALLARI: Makas Kağıdı yener| Kağıt Taşı yener | Taş Makas'ı yener | veya beraberlik olur.
-*/
+function kopeginYasi(insanYasi) {
+  return insanYasi * 7;
+}
 
 function oyun(oyuncu, bilgisayar) {
-  /*buraya kodunu yazabilirsin*/
+  if(oyuncu == "Taş" && bilgisayar == "Makas"){
+    return "Kazandın!"
+  }
+  else if(oyuncu == "Taş" && bilgisayar == "Kağıt"){
+    return "Kaybettin!";
+  }
+  else if(oyuncu == bilgisayar){
+    return "Beraberlik";
+  }else if(oyuncu == "Kağıt" && bilgisayar == "Taş"){ 
+    return "Kazandın!"
+  }else if(oyuncu == "Kağıt" && bilgisayar == "Makas"){
+    return "Kaybettin!"
+  }else if(oyuncu == "Makas" && bilgisayar == "Kağıt"){
+    return "Kazandın!"
+  }else if(oyuncu == "Makas" && bilgisayar == "Taş"){
+    return "Kaybettin!"
+  }
 }
 
-// Şimdi Taş, Kağıt, Makas oyununu bilgisayara karşı oynayalım!
-/*
-Öncelikle aşağıdakileri yap:
-1. Bilgisayarın seçimini rastgele oluşturacağım bir fonksiyon tanımla. Örn: 
-   function bilgisayarinSecimi() {
-   
-   }
-2. Fonsiyonun içinde bilgisayarın seçimi için bir değişken tanımla
-3. Math.random'ı kullanarak bilgisayarın seçimini oluşturun (Math.random 0-1 arasında bir değer verecek)
-4. Bu rastgele değeri "Taş", "Kağıt" veya "Makas"a dönüştüren bir koşul oluşturun
-5. Oluşan değeri geri dönün
-
-Şimdi kendi seçtiğin bir seçime karşı bilgisayarın rastgele oluşturduğu seçimi yukarıda yazdığın oyun fonsiyonu ile oynayın ve sonucu console'a yazdırın.
-Örn: console.log(oyun("Makas",bilgisayarinSecimi()))
-*/
-
-/* Görev 4 : Metrik Dönüştürücü */
-
-//Görev 4a - Kilometreden Mil
-/*
-Aşağdaki milDonusturucu fonksiyonunu aşağıdakileri kullanarak tamamla:
-1. Kilometre değerini argüman olarak alın.
-2. Aldığın bu değeri Mil'e dönüştürün
-3. Mil değerini geri dönün
-*/
-
-function milDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function bilgisayarinSecimi(){
+  let secim;
+  let index = Math.random() * 3;
+  index = Math.ceil(index);
+  console.log(index)
+  if(index == 1){
+    secim = "Makas";
+  }else if(index == 2){
+    secim == "Kağıt";
+  }else if(index == 3){
+    secim == "Taş";
+  } 
+  return secim;
 }
 
-//Görev 4b - Santimetreden Feet
-/*
-Aşağıdakileri feetDonusturucu fonsiyonunu kullanarak yap:
-1. Santimetre değerini argüman olarak alın.
-2. Aldığın bu değeri feet'e dönüştürün
-3. feet değerini geri dönün
 
-Google'da arama ipucu: "feet cm dönüştürme"
-*/
+console.log(oyun("Taş",bilgisayarinSecimi()))
 
-function feetDonusturucu(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+
+function milDonusturucu(km) {
+  return km * 0.621371
 }
 
-/* Görev 5 : 5 küçük maymun yatakta zıplamış şarkısını çocuklar için hazırladığımı varsayalım. https://www.youtube.com/watch?v=e4EJ34xnlxk */
-
-/*
-Aşağıdakileri cocukSarkisi fonksiyonunda yap:
-1. Başlangıçta var olan maymun sayısını alın.
-2. cocukSarkisi fonksiyonu aşağıdaki satırı sadece 1 kere yazacak şekilde kodunuzu yazın:
-
-    "{sayı} küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kmış: Bir daha yatakta zıplamak yok!"
-
-3. Bu fonksiyonun dışında bir yerde, maymun sayısının her seferinde 1 azaldığı ve maymun sayısı 1 olana kadar devem eden bir döngü oluşturun. 
-4. Bu döngüde, her seferinde cocukSarkisi fonsiyonu çalışsın ve console.log'a dönen metni yazdırsın.
-*/
-
-function cocukSarkisi(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function feetDonusturucu(cm) {
+  return cm / 30.48
 }
 
-/* Görev 6 : Not Hesaplayıcı */
+function cocukSarkisi(i) {
+  let sarki = i + " küçük maymun yatakta zıplamış, biri düşüp başını çarpmış, Anne doktoru aramış, Doktor çok kızmış: Bir daha yatakta zıplamak yok!"
+  return sarki
+}
 
-/*
-Aşağdakileri notHesapla fonksiyonunda yap.
-1. 100'lük sistemde bir sınav sonucu alın.
-2. Aşağıdaki tabloya göre notu dönün.
+for(let i = 5 ; i > 0 ; i--){
+  console.log(cocukSarkisi(i))
+}
 
- 90-100 arasında 'A aldın' 
- 80-89 arasında 'B aldın'
- 70-79 arasında 'C aldın'
- 60-69 arasında 'D aldın'
- daha aşağıda 'F aldın'
- dönün
-*/
-
-function notHesapla(/*buraya kodunu yazabilirsin*/) {
-  /*buraya kodunu yazabilirsin*/
+function notHesapla(not) {
+  if(not > 89 && not < 101){
+    return "A aldın"
+  }
+  if(not > 79 && not < 90){
+    return "B aldın"
+  }
+  if(not > 69 && not < 80){
+    return "C aldın"
+  }
+  if(not > 59 && not < 70){
+    return "D aldın"
+  }
+  if(not < 60){
+    return "F aldın"
+  }
+  
 }
 
 /* Bonus Çalışma: Sesli harf sayacı - Kaç tane sesli harf var? */
@@ -182,7 +118,7 @@ function sesliHarfSayaci(/*buraya kodunu yazabilirsin*/) {
   /*buraya kodunu yazabilirsin*/
 }
 
-/* Lütfen bu satırın alt tarafını değiştirmeyin */
+
 function sa() {
   console.log("Kodlar çalışıyor");
   return "as";
